4dff38307fa2aa40867e24cd635499138bfe6450
eldaTacenter <darioguaraz@gmail.com>
2025-03-12 21:00:25 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
-/*Objetivos:
-  -usar funciones
-  -usar variables
-  -usar 1 ciclo
-  -usar 1 array
-*/
+// Seleccionamos los elementos del DOM
+const input = document.getElementById("input");
+const botones = document.querySelectorAll(".btn");
+const titulo = document.querySelector("h2");
+const feedback = document.querySelector("h3");
 
-const numeros_usuario = [];
+// Recuperar el nombre del usuario, o pedirlo y guardarlo
+const nombreUsuario =
+  localStorage.getItem("nombreUsuario") || prompt("Ingrese su nombre:");
+if (nombreUsuario) localStorage.setItem("nombreUsuario", nombreUsuario);
+titulo.textContent = `Hola ${nombreUsuario || "Invitado"}, bienvenido.`;
 
-for (let i = 0; i < 3; i++) {
-  let numero_ingresado = parseInt(
-    prompt(`ingrese valor ${i + 1} de 3 de su boleto de loteria (maximo 50)`)
-  );
-  function verificador_ingresos() {
-    while (numero_ingresado > 50) {
-      alert("ingrese numero valido, entre 0 y 50");
-      numero_ingresado = parseInt(
-        prompt(`ingrese valor ${i + 1}/3 de su boleto de loteria (maximo 50)`)
-      );
-    }
+// Recuperar números guardados en localStorage o inicializar array vacío
+let numerosUsuario = JSON.parse(localStorage.getItem("numerosUsuario")) || [];
+
+// Validar si el numero esta entre 1 y 50
+const validarNumero = (valor) => valor >= 1 && valor <= 50;
+
+// Funcion evento de clic en los botones
+const manejarIntento = (event) => {
+  const numeroIngresado = parseInt(input.value);
+  const boton = event.target;
+
+  if (!validarNumero(numeroIngresado))
+    return alert("Número inválido entre 1 y 50");
+
+  if (numerosUsuario.length < 3) {
+    numerosUsuario.push(numeroIngresado);
+    localStorage.setItem("numerosUsuario", JSON.stringify(numerosUsuario));
+
+    boton.disabled = true;
+    boton.style.backgroundColor = "gray";
   }
-  verificador_ingresos();
 
-  numeros_usuario.push(numero_ingresado);
-}
+  input.value = "";
 
-console.log("tus numeros de boleto ingresados son:");
-for (let i = 0; i < numeros_usuario.length; i++) {
-  numeros_usuario.sort((a, b) => a - b);
-  console.log(numeros_usuario[i]);
-}
-
-const numero_ganador = parseInt(15);
-console.log("el numero ganador es: " + numero_ganador);
-
-let loteria = () => {
-  if (numeros_usuario.includes(numero_ganador)) {
-    console.log("felicidades, has ganado la loteria");
-  } else {
-    console.log("Lo siento pero no ganaste. Segui participando...");
-  }
+  if (numerosUsuario.length === 3) generarNumeroGanador();
 };
-loteria();
+
+// generar el numero ganador y comprobar si se gano
+const generarNumeroGanador = () => {
+  const numeroGanador = Math.floor(Math.random() * 50) + 1;
+  localStorage.setItem("numeroGanador", numeroGanador);
+  loteria(numeroGanador);
+};
+
+// verificar si el usuario gano
+const loteria = (numeroGanador) => {
+  const mensaje = numerosUsuario.includes(numeroGanador)
+    ? `¡Caracoles, santos recorcholis y la marencoche! has ganado la lotería! El número ganador es: ${numeroGanador}`
+    : `Segui participando. El número ganador era el: ${numeroGanador}`;
+
+  console.log(mensaje);
+  feedback.textContent = mensaje;
+};
+
+// Agregar eventos a los botones
+botones.forEach((boton) => boton.addEventListener("click", manejarIntento));
